59adba4e17605920fbd33da474239411b594ce86
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2021-06-16 23:16:41 +0800

    Update

--- pyKinectAzure/record.py
+++ pyKinectAzure/record.py
@@ -63,120 +63,120 @@ class load_record:
 	def capture_get_depth_image(self):
 		return(self.k4a.k4a_capture_get_depth_image(self.capture_handle))
 	
-	def image_get_buffer(self, image_handle):
-		"""Get the image buffer.
+	# def image_get_buffer(self, image_handle):
+	# 	"""Get the image buffer.
 
-		Parameters:
-		image_handle (k4a_image_t): Handle to the Image
+	# 	Parameters:
+	# 	image_handle (k4a_image_t): Handle to the Image
 
-		Returns:
-		ctypes.POINTER(ctypes.c_uint8): The function will return NULL if there is an error, and will normally return a pointer to the image buffer.
-										Since all k4a_image_t instances are created with an image buffer, this function should only return NULL if the
-										image_handle is invalid.
+	# 	Returns:
+	# 	ctypes.POINTER(ctypes.c_uint8): The function will return NULL if there is an error, and will normally return a pointer to the image buffer.
+	# 									Since all k4a_image_t instances are created with an image buffer, this function should only return NULL if the
+	# 									image_handle is invalid.
 
-		Remarks:
-		Use this buffer to access the raw image data.
-		"""
+	# 	Remarks:
+	# 	Use this buffer to access the raw image data.
+	# 	"""
 
-		return self.k4a.k4a_image_get_buffer(image_handle)
+	# 	return self.k4a.k4a_image_get_buffer(image_handle)
 
-	def image_get_size(self, image_handle):
-		"""Get the image buffer size.
+	# def image_get_size(self, image_handle):
+	# 	"""Get the image buffer size.
 
-		Parameters:
-		image_handle (k4a_image_t): Handle to the Image
+	# 	Parameters:
+	# 	image_handle (k4a_image_t): Handle to the Image
 
-		Returns:
-		int: The function will return 0 if there is an error, and will normally return the image size.
-		Since all k4a_image_t instances are created with an image buffer, this function should only return 0 if the
-		image_handle is invalid.
+	# 	Returns:
+	# 	int: The function will return 0 if there is an error, and will normally return the image size.
+	# 	Since all k4a_image_t instances are created with an image buffer, this function should only return 0 if the
+	# 	image_handle is invalid.
 
-		Remarks:
-		Use this function to know what the size of the image buffer is returned by k4a_image_get_buffer().
-		"""
+	# 	Remarks:
+	# 	Use this function to know what the size of the image buffer is returned by k4a_image_get_buffer().
+	# 	"""
 
-		return int(self.k4a.k4a_image_get_size(image_handle))
+	# 	return int(self.k4a.k4a_image_get_size(image_handle))
 	
-	def image_get_width_pixels(self, image_handle):
-		"""Get the image width in pixels.
+	# def image_get_width_pixels(self, image_handle):
+	# 	"""Get the image width in pixels.
 
-		Parameters:
-		image_handle (k4a_image_t): Handle to the Image
+	# 	Parameters:
+	# 	image_handle (k4a_image_t): Handle to the Image
 
-		Returns:
-		int: This function is not expected to fail, all k4a_image_t's are created with a known width. If the part
-		image_handle is invalid, the function will return 0.
-		"""
+	# 	Returns:
+	# 	int: This function is not expected to fail, all k4a_image_t's are created with a known width. If the part
+	# 	image_handle is invalid, the function will return 0.
+	# 	"""
 
-		return int(self.k4a.k4a_image_get_width_pixels(image_handle))
+	# 	return int(self.k4a.k4a_image_get_width_pixels(image_handle))
 
-	def image_get_height_pixels(self, image_handle):
-		"""Get the image height in pixels.
+	# def image_get_height_pixels(self, image_handle):
+	# 	"""Get the image height in pixels.
 
-		Parameters:
-		image_handle (k4a_image_t): Handle to the Image
+	# 	Parameters:
+	# 	image_handle (k4a_image_t): Handle to the Image
 
-		Returns:
-		int: This function is not expected to fail, all k4a_image_t's are created with a known height. If the part
-		image_handle is invalid, the function will return 0.
-		"""
+	# 	Returns:
+	# 	int: This function is not expected to fail, all k4a_image_t's are created with a known height. If the part
+	# 	image_handle is invalid, the function will return 0.
+	# 	"""
 
-		return int(self.k4a.k4a_image_get_height_pixels(image_handle))
+	# 	return int(self.k4a.k4a_image_get_height_pixels(image_handle))
 	
-	def image_get_format(self, image_handle):
-		"""Get the format of the image.
+	# def image_get_format(self, image_handle):
+	# 	"""Get the format of the image.
 
-		Parameters:
-		image_handle (k4a_image_t): Handle to the Image
+	# 	Parameters:
+	# 	image_handle (k4a_image_t): Handle to the Image
 
-		Returns:
-		int: This function is not expected to fail, all k4a_image_t's are created with a known format. If the
-		image_handle is invalid, the function will return ::K4A_IMAGE_FORMAT_CUSTOM.
+	# 	Returns:
+	# 	int: This function is not expected to fail, all k4a_image_t's are created with a known format. If the
+	# 	image_handle is invalid, the function will return ::K4A_IMAGE_FORMAT_CUSTOM.
 
-		Remarks:
-		Use this function to determine the format of the image buffer.
-		"""
+	# 	Remarks:
+	# 	Use this function to determine the format of the image buffer.
+	# 	"""
 
-		return int(self.k4a.k4a_image_get_format(image_handle))
+	# 	return int(self.k4a.k4a_image_get_format(image_handle))
 
-	def image_convert_to_numpy(self, image_handle):
-		"""Get the image data as a numpy array
+	# def image_convert_to_numpy(self, image_handle):
+	# 	"""Get the image data as a numpy array
 
-		Parameters:
-		image_handle (k4a_image_t): Handle to the Image
+	# 	Parameters:
+	# 	image_handle (k4a_image_t): Handle to the Image
 
-		Returns:
-		numpy.ndarray: Numpy array with the image data
-		"""
+	# 	Returns:
+	# 	numpy.ndarray: Numpy array with the image data
+	# 	"""
 
-		# Get the pointer to the buffer containing the image data
-		buffer_pointer = self.image_get_buffer(image_handle)
+	# 	# Get the pointer to the buffer containing the image data
+	# 	buffer_pointer = self.image_get_buffer(image_handle)
 
-		# Get the size of the buffer
-		image_size = self.image_get_size(image_handle)
-		image_width = self.image_get_width_pixels(image_handle)
-		image_height = self.image_get_height_pixels(image_handle)
+	# 	# Get the size of the buffer
+	# 	image_size = self.image_get_size(image_handle)
+	# 	image_width = self.image_get_width_pixels(image_handle)
+	# 	image_height = self.image_get_height_pixels(image_handle)
 
-		# Get the image format
-		image_format = self.image_get_format(image_handle)
+	# 	# Get the image format
+	# 	image_format = self.image_get_format(image_handle)
 
-		# Read the data in the buffer
-		buffer_array = np.ctypeslib.as_array(buffer_pointer,shape=(image_size,))
+	# 	# Read the data in the buffer
+	# 	buffer_array = np.ctypeslib.as_array(buffer_pointer,shape=(image_size,))
 
-		# Parse buffer based on image format
-		if image_format == _k4a.K4A_IMAGE_FORMAT_COLOR_MJPG:
-			return cv2.imdecode(np.frombuffer(buffer_array, dtype=np.uint8), -1)
-		elif image_format == _k4a.K4A_IMAGE_FORMAT_COLOR_NV12:
-			yuv_image = np.frombuffer(buffer_array, dtype=np.uint8).reshape(int(image_height*1.5),image_width)
-			return cv2.cvtColor(yuv_image, cv2.COLOR_YUV2BGR_NV12)
-		elif image_format == _k4a.K4A_IMAGE_FORMAT_COLOR_YUY2:
-			yuv_image = np.frombuffer(buffer_array, dtype=np.uint8).reshape(image_height,image_width,2)
-			return cv2.cvtColor(yuv_image, cv2.COLOR_YUV2BGR_YUY2)
-		elif image_format == _k4a.K4A_IMAGE_FORMAT_COLOR_BGRA32:
-			return np.frombuffer(buffer_array, dtype=np.uint8).reshape(image_height,image_width,4)
-		elif image_format == _k4a.K4A_IMAGE_FORMAT_DEPTH16:
-			return np.frombuffer(buffer_array, dtype="<u2").reshape(image_height,image_width)#little-endian 16 bits unsigned Depth data
-		elif image_format == _k4a.K4A_IMAGE_FORMAT_IR16:
-			return np.frombuffer(buffer_array, dtype="<u2").reshape(image_height,image_width)#little-endian 16 bits unsigned IR data. For more details see: https://microsoft.github.io/Azure-Kinect-Sensor-SDK/release/1.2.x/namespace_microsoft_1_1_azure_1_1_kinect_1_1_sensor_a7a3cb7a0a3073650bf17c2fef2bfbd1b.html
-		elif image_format == _k4a.K4A_IMAGE_FORMAT_CUSTOM8:
-			return np.frombuffer(buffer_array, dtype="<u1").reshape(image_height,image_width)
+	# 	# Parse buffer based on image format
+	# 	if image_format == _k4a.K4A_IMAGE_FORMAT_COLOR_MJPG:
+	# 		return cv2.imdecode(np.frombuffer(buffer_array, dtype=np.uint8), -1)
+	# 	elif image_format == _k4a.K4A_IMAGE_FORMAT_COLOR_NV12:
+	# 		yuv_image = np.frombuffer(buffer_array, dtype=np.uint8).reshape(int(image_height*1.5),image_width)
+	# 		return cv2.cvtColor(yuv_image, cv2.COLOR_YUV2BGR_NV12)
+	# 	elif image_format == _k4a.K4A_IMAGE_FORMAT_COLOR_YUY2:
+	# 		yuv_image = np.frombuffer(buffer_array, dtype=np.uint8).reshape(image_height,image_width,2)
+	# 		return cv2.cvtColor(yuv_image, cv2.COLOR_YUV2BGR_YUY2)
+	# 	elif image_format == _k4a.K4A_IMAGE_FORMAT_COLOR_BGRA32:
+	# 		return np.frombuffer(buffer_array, dtype=np.uint8).reshape(image_height,image_width,4)
+	# 	elif image_format == _k4a.K4A_IMAGE_FORMAT_DEPTH16:
+	# 		return np.frombuffer(buffer_array, dtype="<u2").reshape(image_height,image_width)#little-endian 16 bits unsigned Depth data
+	# 	elif image_format == _k4a.K4A_IMAGE_FORMAT_IR16:
+	# 		return np.frombuffer(buffer_array, dtype="<u2").reshape(image_height,image_width)#little-endian 16 bits unsigned IR data. For more details see: https://microsoft.github.io/Azure-Kinect-Sensor-SDK/release/1.2.x/namespace_microsoft_1_1_azure_1_1_kinect_1_1_sensor_a7a3cb7a0a3073650bf17c2fef2bfbd1b.html
+	# 	elif image_format == _k4a.K4A_IMAGE_FORMAT_CUSTOM8:
+	# 		return np.frombuffer(buffer_array, dtype="<u1").reshape(image_height,image_width)
--- read_record.py
+++ read_record.py
@@ -11,23 +11,44 @@ import cv2
 # Path to the module
 # TODO: Modify with the path containing the k4a.dll from the Azure Kinect SDK
 modulePath = 'C:\\Program Files\\Azure Kinect SDK v1.4.1\\sdk\\windows-desktop\\amd64\\release\\bin\\k4a.dll' 
+bodyTrackingModulePath = 'C:\\Program Files\\Azure Kinect Body Tracking SDK\\sdk\\windows-desktop\\amd64\\release\\bin\\k4abt.dll'
 # under x86_64 linux please use r'/usr/lib/x86_64-linux-gnu/libk4a.so'
 # In Jetson please use r'/usr/lib/aarch64-linux-gnu/libk4a.so'
+
 
 if __name__ == "__main__":
 
 	pyK4A = pyKinectAzure.pyKinectAzure(modulePath)	# 必須載入k4a.dll才不會出錯
 	recorder = record.load_record(modulePath)
+	
+	# Open record
 	recorder.playback_open('./record/test.mkv')
 	recorder.get_capture()
+	pyK4A.device_open()
+	pyK4A.capture_handle = recorder.capture_handle
+	
 
+	# Modify camera configuration
+	device_config = pyK4A.config
+	device_config.color_resolution = _k4a.K4A_COLOR_RESOLUTION_OFF
+	device_config.depth_mode = _k4a.K4A_DEPTH_MODE_NFOV_UNBINNED
+	print(device_config)
+
+
+	# Initialize the body tracker
+	pyK4A.bodyTracker_start(bodyTrackingModulePath)
+
+	
 	# Get the depth image from the capture
-	depth_image_handle = recorder.capture_get_depth_image()
+	depth_image_handle = pyK4A.capture_get_depth_image()
 	if depth_image_handle:
 		# Read and convert the image data to numpy array:
-		depth_image = recorder.image_convert_to_numpy(depth_image_handle)
+		depth_image = pyK4A.image_convert_to_numpy(depth_image_handle)
 		depth_color_image = cv2.convertScaleAbs (depth_image, alpha=0.05)  #alpha is fitted by visual comparison with Azure k4aviewer results  
 		depth_color_image = cv2.applyColorMap(depth_color_image, cv2.COLORMAP_JET)
 		cv2.namedWindow('Colorized Depth Image',cv2.WINDOW_NORMAL)
 		cv2.imshow('Colorized Depth Image',depth_color_image)
 		k = cv2.waitKey(1)
+
+		# Get body segmentation image
+		# body_image_color = pyK4A.bodyTracker_get_body_segmentation()
